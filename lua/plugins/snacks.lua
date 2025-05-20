218890f09574fe36933552c0f36011e7d7788468
Adam <8365885+zvakanaka@users.noreply.github.com>
2025-05-20 10:07:16 -0600

    support images in snacks

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,18 @@
+-- stylua: ignore
+-- if true then return {} end
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    image = {
+      -- your image configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    lazygit = {
+      -- your lazygit configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  },
+}
